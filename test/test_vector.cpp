73ad279795948cd3b6414cedc06b9b6ae757c0dd
rbcheng <rbcheng@qq.com>
2018-11-12 21:15:06 +0800

    init project

---
+++ test/test_vector.cpp
@@ -0,0 +1,16 @@
+//
+// Created by rbcheng on 18-11-12.
+// Email: rbcheng@qq.com
+//
+
+#include "vector"
+
+int main() {
+    stdd::vector<int > v(2, 9);
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+
+
+}
